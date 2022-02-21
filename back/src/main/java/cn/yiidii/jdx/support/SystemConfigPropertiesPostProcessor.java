a2febc1f130cd616126b6fe449dc074c0119adb3
wangyidi0731 <wangyiidii@163.com>
2022-02-22 01:42:12 +0800

    启动后对配置文件检查

--- back/src/main/java/cn/yiidii/jdx/support/SystemConfigPropertiesPostProcessor.java
+++ back/src/main/java/cn/yiidii/jdx/support/SystemConfigPropertiesPostProcessor.java
@@ -1,6 +1,12 @@
 package cn.yiidii.jdx.support;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.yiidii.jdx.config.prop.SystemConfigProperties;
+import cn.yiidii.jdx.config.prop.SystemConfigProperties.QLConfig;
+import cn.yiidii.jdx.config.prop.SystemConfigProperties.SocialPlatform;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
@@ -18,18 +24,27 @@ public class SystemConfigPropertiesPostProcessor implements BeanPostProcessor {
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
         if (bean instanceof SystemConfigProperties) {
             // 检查
-//            List<RobotConfig> robotConfigs = ((SystemConfigProperties) bean).getRobot();
-//            Set<Long> configuredRobotQqs = robotConfigs.stream().map(RobotConfig::getQq).collect(Collectors.toSet());
-//            Set<String> configuredRobotNames = robotConfigs.stream().map(RobotConfig::getName).collect(Collectors.toSet());
-//            String errMessage =
-//                    robotConfigs.size() != configuredRobotQqs.size() ? "配置的机器人QQ存在重复, 请检查! 系统退出..." :
-//                            robotConfigs.size() != configuredRobotNames.size() ? "配置的机器人名称存在重复, 请检查! 系统退出..." : "";
-//
-//            if (StrUtil.isNotBlank(errMessage)) {
-//                log.error(errMessage);
-//                throw new RuntimeException(errMessage);
-//            }
+            this.checkQLs(((SystemConfigProperties) bean).getQls());
+            this.checkSocialPlatforms(((SystemConfigProperties) bean).getSocialPlatforms());
         }
         return BeanPostProcessor.super.postProcessAfterInitialization(bean, beanName);
+    }
+
+    private void checkQLs(List<QLConfig> qlConfigs) {
+        // 检查QL名称重复
+        List<String> duplicateDisplayName = qlConfigs.stream().collect(Collectors.groupingBy(QLConfig::getDisplayName))
+                .entrySet().stream().filter(e -> e.getValue().size() > 1).map(e -> e.getKey()).collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(duplicateDisplayName)) {
+            throw new RuntimeException(StrUtil.format("青龙配置异常, 以下名称出现重复: {}", CollUtil.join(duplicateDisplayName, ",")));
+        }
+    }
+
+    private void checkSocialPlatforms(List<SocialPlatform> socialPlatforms) {
+        // 检查社交配置
+        List<String> duplicateDisplayName = socialPlatforms.stream().collect(Collectors.groupingBy(SocialPlatform::getSource))
+                .entrySet().stream().filter(e -> e.getValue().size() > 1).map(e -> e.getKey()).collect(Collectors.toList());
+        if (CollUtil.isNotEmpty(duplicateDisplayName)) {
+            throw new RuntimeException(StrUtil.format("社交登录信息配置异常, 以下平台配置重复: {}", CollUtil.join(duplicateDisplayName, ",")));
+        }
     }
 }
