2687bbf6a320574572b2c8923c935b2962c967f8
wangyidi0731 <wangyiidii@163.com>
2022-02-24 22:20:17 +0800

    属性初始化

--- back/src/main/java/cn/yiidii/jdx/config/prop/SystemConfigProperties.java
+++ back/src/main/java/cn/yiidii/jdx/config/prop/SystemConfigProperties.java
@@ -43,7 +43,7 @@ public class SystemConfigProperties implements InitializingBean {
 
     private String title;
     private String notice;
-    private String noticeModel;
+    private String noticeModel = "TOP";
     private List<SocialPlatform> socialPlatforms;
     private List<QLConfig> qls;
 
