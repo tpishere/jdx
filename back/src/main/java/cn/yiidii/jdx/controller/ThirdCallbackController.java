8d021819093f90a3fdc06355071f64b666ff5d15
wangyidi0731 <wangyiidii@163.com>
2022-02-28 13:59:13 +0800

    青龙通知回调

--- back/src/main/java/cn/yiidii/jdx/controller/ThirdCallbackController.java
+++ back/src/main/java/cn/yiidii/jdx/controller/ThirdCallbackController.java
@@ -8,11 +8,7 @@ import cn.yiidii.jdx.model.ex.BizException;
 import cn.yiidii.jdx.util.WXPushUtil;
 import com.alibaba.fastjson.JSONObject;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.Assert;
@@ -69,37 +65,24 @@ public class ThirdCallbackController {
     private void handle(JSONObject paramJo) {
         String text = paramJo.getString("text");
         String desp = paramJo.getString("desp");
-        Assert.isTrue(StrUtil.isNotBlank(text) && StrUtil.isNotBlank(desp), () -> {
+        String ptPin = paramJo.getString("ptPin");
+        Assert.isTrue(StrUtil.isNotBlank(text) && StrUtil.isNotBlank(desp) && StrUtil.isNotBlank(ptPin), () -> {
             throw new BizException("参数错误");
         });
 
         // 优先使用ql回调时传入的wxAppToken，没传则使用JDX配置的wxAppToken
         String wxPusherAppToken = paramJo.getString("wxPusherAppToken");
         wxPusherAppToken = StrUtil.isNotBlank(wxPusherAppToken) ? wxPusherAppToken : jdUserConfigProperties.getAppToken();
-        switch (text) {
-            case "京东资产变动通知": {
-                List<String> userContentList = Arrays.stream(desp.split("\\n\\n")).collect(Collectors.toList()).stream().filter(s -> StrUtil.isNotBlank(s) && !s.contains("本通知")).collect(Collectors.toList());
-                Map<String, String> userContentMap = userContentList.stream().collect(Collectors.toMap(e -> {
-                    String s = e.split("\\n")[1];
-                    return s.replaceAll("账号名称：", "");
-                }, e -> e, (e1, e2) -> e2));
-                log.debug("京东资产变动通知, 根据回调信息汇总的mapping: {}", JSONObject.toJSONString(userContentMap));
-
-                for (Entry<String, String> entry : userContentMap.entrySet()) {
-                    String k = entry.getKey();
-                    String v = entry.getValue();
-                    JDUserConfig byPtPin = jdUserConfigProperties.getByPtPin(k);
-                    if (Objects.isNull(byPtPin)) {
-                        continue;
-                    }
-                    WXPushUtil.send(wxPusherAppToken, Arrays.asList(byPtPin.getWxPusherUid()), text, v, "1");
-                }
-                break;
-            }
-            default: {
-                return;
-            }
+        if (StrUtil.isBlank(wxPusherAppToken)) {
+            log.info(StrUtil.format("[{}], {}, 缺少wxPusherAppToken, 取消通知", ptPin, text));
+            return;
         }
+
+        JDUserConfig byPtPin = jdUserConfigProperties.getByPtPin(ptPin);
+        if (Objects.isNull(byPtPin)) {
+            return;
+        }
+        WXPushUtil.send(wxPusherAppToken, Arrays.asList(byPtPin.getWxPusherUid()), text, desp, "1");
     }
 
 }
